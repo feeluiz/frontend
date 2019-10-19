4129134cd04884afdd30670c71b6c813823c2644
feeluiz <feluiz95@gmail.com>
2019-10-19 12:34:34 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Routes from './routes'
-require('dotenv').config()
+
 
 
 function App() {
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: "https://backend-pic.herokuapp.com",
     timeout: 7000,
 })
 
